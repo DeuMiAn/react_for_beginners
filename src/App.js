b354e5ec4d223a61d548cbbe3d28a8b6e4ca88e2
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2021-12-24 05:21:47 +0900

    Coin tracker finished

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
 import {useState, useEffect} from "react";
 
-// function App() {
-//   const [counter, setValue]=useState(0);
-//   const [keyword,setKeyword]=useState("");
-//   const onClick=()=>setValue((prev)=>prev+1);
-//   const onChange=(event)=>setKeyword(event.target.value);
-//   useEffect(()=>{
-//     console.log("when both change Run");
-//   },[keyword,counter]);
-//   useEffect(()=>{
-//     console.log("when 'counter' change i run")
-//   },[counter]);
-//   useEffect(()=>{
-//     console.log("when 'keybord'change i run");
-//   },[keyword])
-  
-//   return (
-//     <div>
-//       <input value={keyword}
-//        type="text"
-//         onChange={onChange} 
-//         placeholder="Search here..."/>
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>click me</button>
-//     </div>
-//   );
-// }  //---------useEffect1
-function Hello(){
-  useEffect(()=>{
-    console.log("created! ;)");
-return ()=>console.log("destroyed :(");},[])
-  return <h1>Hello</h1>
-}
 function App(){
-  const [showing, setShowing]=useState(false);
-  const onClick=()=>setShowing((prev)=>!prev);
-  return (
-    <div>
-      {showing ? <Hello/>:null}
-      <button onClick={onClick}>{showing ? "hide":"show"}</button>
-    </div>
+  const [loading, setLoading]=useState(true);
+  const [coins,setCoins]=useState([]);
+  useEffect(
+    ()=>{
+      fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res)=>res.json()
+      .then((json)=>{
+        setCoins(json);
+        setLoading(false);
+      }));
+    },[]
   )
+  return <div>
+    <h1>-가상코인{loading ? "" : `(${coins.length})`}-</h1>
+    {loading ? <strong>Loding...</strong> : <select>
+      {coins.map((coin)=>(
+      <option key={coin.id}>
+        {coin.name} ({coin.symbol}):${coin.quotes.USD.price} USD
+      </option>
+      ))}
+    </select>}
+    
+  </div>
 }
 
 export default App;
