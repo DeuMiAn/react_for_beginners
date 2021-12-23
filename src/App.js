75fe73f999f197a3dd7e76b801f8af6f83181d1d
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2021-12-24 04:51:15 +0900

    toDoList finished

--- src/App.js
+++ src/App.js
@@ -1,46 +1,34 @@
-import {useState, useEffect} from "react";
+import { useState } from "react";
 
-// function App() {
-//   const [counter, setValue]=useState(0);
-//   const [keyword,setKeyword]=useState("");
-//   const onClick=()=>setValue((prev)=>prev+1);
-//   const onChange=(event)=>setKeyword(event.target.value);
-//   useEffect(()=>{
-//     console.log("when both change Run");
-//   },[keyword,counter]);
-//   useEffect(()=>{
-//     console.log("when 'counter' change i run")
-//   },[counter]);
-//   useEffect(()=>{
-//     console.log("when 'keybord'change i run");
-//   },[keyword])
-  
-//   return (
-//     <div>
-//       <input value={keyword}
-//        type="text"
-//         onChange={onChange} 
-//         placeholder="Search here..."/>
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>click me</button>
-//     </div>
-//   );
-// }  //---------useEffect1
-function Hello(){
-  useEffect(()=>{
-    console.log("created! ;)");
-return ()=>console.log("destroyed :(");},[])
-  return <h1>Hello</h1>
-}
 function App(){
-  const [showing, setShowing]=useState(false);
-  const onClick=()=>setShowing((prev)=>!prev);
-  return (
-    <div>
-      {showing ? <Hello/>:null}
-      <button onClick={onClick}>{showing ? "hide":"show"}</button>
-    </div>
-  )
+  const [toDo,setToDO]=useState("");
+  const [toDos,setToDos]=useState([]);
+  const onChange=(event)=>setToDO(event.target.value);
+  const onSubmit=(event)=>{
+    event.preventDefault();
+    if(toDo===""){
+      return;
+    }
+    setToDO("");
+    setToDos((curArr)=>[toDo, ...curArr]);
+    
+  }
+  return <div>
+    <h1>My To Dos({toDos.length})</h1>
+   <form onSubmit={onSubmit}>
+   <input 
+    onChange={onChange} 
+    value={toDo}
+     type="text"
+      placeholder="Write yout to do"/>
+      <button>
+        Add To Do
+      </button>
+   </form>
+   <hr />
+   <ul>
+   {toDos.map((item, index)=><li key={index}>{item}</li>)}
+   </ul>
+  </div>
 }
-
 export default App;
